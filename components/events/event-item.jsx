02e8d518c139e0d059ebed68736b50b56699e47c
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-03 11:56:08 +0430

    Add event list

---
+++ components/events/event-item.jsx
@@ -0,0 +1,51 @@
+// import Image from "next/image";
+import Image from "next/legacy/image";
+import Link from "next/link";
+
+import styles from "./event-item.module.css";
+import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+
+const EventItem = ({ event }) => {
+  const { id, title, location, date, image } = event;
+
+  const readableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const address = location.replace(", ", "\n");
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={styles.item}>
+      <Image src={`/${image}`} alt={title} width={360} height={240} />
+      <div className={styles.content}>
+        <div>
+          <h2>{title}</h2>
+          <div className={styles.date}>
+            <DateIcon />
+            <time>{readableDate}</time>
+          </div>
+          <div className={styles.address}>
+            <AddressIcon />
+            <address>{address}</address>
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
