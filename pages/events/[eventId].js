2b4dd2e3a02ad215590509419a3f0f7d841f7e14
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-07 23:09:04 +0430

    Add static props and make SSG

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,13 +1,9 @@
 import EventLogistics from "@/components/event-detail/event-logistics";
 import EventSummary from "@/components/event-detail/event-summary";
 import EventContent from "@/components/event-detail/event-content";
-import { getEventById } from "@/dummy-data";
-import { useRouter } from "next/router";
+import { getEventById, getFeaturedEvents } from "@/helpers/api-utils";
 
-function EventDetailPage() {
-  const router = useRouter();
-  const event = getEventById(router.query.eventId);
-
+function EventDetailPage({ event }) {
   if (!event) return <p className="center">NO event found!</p>;
 
   return (
@@ -22,3 +18,28 @@ function EventDetailPage() {
 }
 
 export default EventDetailPage;
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const eventData = await getEventById(eventId);
+
+  return {
+    props: {
+      event: eventData[0],
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  // We do not generate routes for all events.
+  // Future events are fewer in number.
+  const events = await getFeaturedEvents();
+  const paths = events.map((event) => ({
+    params: { eventId: String(event.id) },
+  }));
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+}
