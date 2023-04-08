3d0e4bfb82f9c60591b07de100e6c712371a6851
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-08 14:16:04 +0430

    Add error block

--- helpers/api-utils.js
+++ helpers/api-utils.js
@@ -1,33 +1,51 @@
 export async function getAllEvents() {
-  const res = await fetch(process.env.DB_URL, {
-    headers: {
-      authorization: `Bearer ${process.env.DB_API_KEY}`,
-      apikey: process.env.DB_API_KEY,
-    },
-  });
-  const data = await res.json();
+  let data = [];
+  try {
+    const res = await fetch(process.env.DB_URL, {
+      headers: {
+        authorization: `Bearer ${process.env.DB_API_KEY}`,
+        apikey: process.env.DB_API_KEY,
+      },
+    });
+    data = await res.json();
+  } catch (error) {
+    // TODO: Handle fetch error
+    console.log(error);
+  }
   return data;
 }
 
 export async function getFeaturedEvents() {
-  const res = await fetch(`${process.env.DB_URL}?isFeatured=eq.true`, {
-    headers: {
-      authorization: `Bearer ${process.env.DB_API_KEY}`,
-      apikey: process.env.DB_API_KEY,
-    },
-  });
-  const data = await res.json();
+  let data = [];
+  try {
+    const res = await fetch(`${process.env.DB_URL}?isFeatured=eq.true`, {
+      headers: {
+        authorization: `Bearer ${process.env.DB_API_KEY}`,
+        apikey: process.env.DB_API_KEY,
+      },
+    });
+    data = await res.json();
+  } catch (error) {
+    // TODO: Handle fetch error
+    console.log(error);
+  }
   return data;
 }
 
 export async function getEventById(id) {
-  const res = await fetch(`${process.env.DB_URL}?id=eq.${id}`, {
-    headers: {
-      authorization: `Bearer ${process.env.DB_API_KEY}`,
-      apikey: process.env.DB_API_KEY,
-    },
-  });
-  const data = await res.json();
+  let data = [];
+  try {
+    const res = await fetch(`${process.env.DB_URL}?id=eq.${id}`, {
+      headers: {
+        authorization: `Bearer ${process.env.DB_API_KEY}`,
+        apikey: process.env.DB_API_KEY,
+      },
+    });
+    data = await res.json();
+  } catch (error) {
+    // TODO: Handle fetch error
+    console.log(error);
+  }
   return data;
 }
 
@@ -36,7 +54,10 @@ export async function getFilteredEvents(dateFilter) {
   const events = await getAllEvents();
 
   let filteredEvents = events.filter((event) => {
-    event.date.getFullYear() === year && event.date.getMonth() === month - 1;
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
   });
 
   return filteredEvents;
