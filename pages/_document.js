236c1c410641430596988084fe34b6361b48f99a
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-09 12:19:24 +0430

    Add Html structure

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body></body>
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
