1d3eab1af4d0c92193acb20088c3908afd826759
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-16 22:42:52 +0430

    Add database functions

---
+++ helpers/db-utils.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.MONGODB_URL);
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db("next-events");
+  const result = await db.collection(collection).insertOne(document);
+
+  return result;
+}
+
+export async function getDocument(client, collection, filter = {}, sort = {}) {
+  const db = client.db("next-events");
+
+  const results = await db
+    .collection(collection)
+    .find(filter)
+    .sort(sort)
+    .toArray();
+
+  return results;
+}
