35972500a277faf919bf6d28e77c2a21edc6f370
Yousef Bigdeli <yousefbigdeli96@gmail.com>
2023-04-16 22:49:03 +0430

    Edit database actions

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,39 +1,43 @@
-import { MongoClient } from "mongodb";
-
-const client = new MongoClient(
-  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gcoji.mongodb.net/?retryWrites=true&w=majority`
-);
+import { connectDatabase, insertDocument } from "@/helpers/db-utils";
 
 async function handler(req, res) {
-  if (req.method === "POST") {
-    const userEmail = req.body.email;
+  let client;
 
-    if (!userEmail || !userEmail.includes("@")) {
+  // connect to the database
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    console.log("Connecting to the database failed!", error);
+    res.status(500).json({ message: "server error.", status: 500 });
+    return;
+  }
+
+  if (req.method === "POST") {
+    const { email } = req.body;
+
+    // Validation of sent email
+    if (!email || !email.includes("@")) {
       res.status(422).json({ message: "Invalid email address.", status: 422 });
+      client.close();
       return;
     }
 
     try {
-      await client.connect();
-      const db = client.db("next-events");
-      await db.collection("newsletter").insertOne({
-        email: userEmail,
-      });
-
+      await insertDocument(client, "newsletter", { email });
       res.status(201).json({
         message: "Signed up!",
         status: 201,
       });
     } catch (error) {
-      console.error(error);
+      console.error("Error insert new email. ", error);
       res.status(500).json({
         message: "Server Error.",
         status: 500,
       });
-    } finally {
-      client.close();
     }
   }
+
+  client.close();
 }
 
 export default handler;
